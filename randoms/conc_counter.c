0fdb78eb1fe250161a9af693cfcd55d2d824c38d
aatbip <anantabipal2010@gmail.com>
2025-12-06 15:32:17 +0545

    multiple thread vs single thread counter experiment

--- randoms/conc_counter.c
+++ randoms/conc_counter.c
@@ -6,6 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double get_time_ms() {
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  return (double)ts.tv_sec * 1000.0 + (double)ts.tv_nsec / 1000000.0;
+}
+
 void Pthread_mutex_init(pthread_mutex_t *mutex) {
   int i = pthread_mutex_init(mutex, NULL);
   assert(i == 0);
@@ -48,18 +54,53 @@ int getcount(counter_t *c) {
   return count;
 }
 
+void *routine(void *t) {
+  for (int i = 0; i < 1000000; i++) {
+    incr((counter_t *)(t));
+  }
+  return NULL;
+}
+
+void Pthread_create(pthread_t *th, void *p) {
+  int i = pthread_create(th, NULL, routine, p);
+  assert(i == 0);
+}
+void Pthread_join(pthread_t th) {
+  int i = pthread_join(th, NULL);
+  assert(i == 0);
+}
+
+void run_threads(int n, counter_t *counter) {
+  pthread_t *th = malloc(sizeof(pthread_t) * n);
+
+  for (int i = 0; i < n; i++) {
+    Pthread_create(&th[i], counter);
+  }
+
+  for (int i = 0; i < n; i++) {
+    Pthread_join(th[i]);
+  }
+
+  free(th);
+}
+
 int main(void) {
   counter_t *counter = malloc(sizeof(*counter));
   if (!counter)
     return 1;
 
-  init(counter);
-  incr(counter);
-  incr(counter);
-  incr(counter);
-  incr(counter);
-  decr(counter);
+  double start = get_time_ms();
+  run_threads(1, counter);
+  double end = get_time_ms();
+
+  // double start = get_time_ms();
+  // for (int i = 0; i < 2000000; i++) {
+  //   incr(counter);
+  // }
+  // double end = get_time_ms();
+
   printf("count: %d\n", getcount(counter));
+  printf("time taken: %f\n", end - start);
 
   return 0;
 }
