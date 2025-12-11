a1c6aa361d124d7a75b08399785c878814954e2b
aatbip <anantabipal2010@gmail.com>
2025-12-11 13:36:56 +0545

    Note: sem_init as lock

--- randoms/semaphore-0.c
+++ randoms/semaphore-0.c
@@ -1,4 +1,8 @@
+/*Binary semaphore - semaphore as locks
+ * We can use semaphore as a lock where we initialize the semaphore with X as 2.
+ * sem_init($s, 0, X) where X = 2 which will act as a lock to protect critical section.*/
 #include <semaphore.h>
+#include <stdio.h>
 
 int main(void) {
   sem_t s;
@@ -6,4 +10,7 @@ int main(void) {
    * Second parameter is 0 indicating that this semaphore will be
    * used within the threads created from the same process.*/
   sem_init(&s, 0, 1);
+  int p = sem_wait(&s);
+  int q = sem_post(&s);
+  printf("p: %d, q: %d\n", p, q);
 }
