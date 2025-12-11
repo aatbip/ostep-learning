386eb6272a64ebfc00828d3e88d029efbf5a078a
aatbip <anantabipal2010@gmail.com>
2025-12-11 13:46:07 +0545

    corrected X value in note and add spwan_thread

--- randoms/semaphore-0.c
+++ randoms/semaphore-0.c
@@ -1,8 +1,32 @@
 /*Binary semaphore - semaphore as locks
- * We can use semaphore as a lock where we initialize the semaphore with X as 2.
- * sem_init($s, 0, X) where X = 2 which will act as a lock to protect critical section.*/
+ * We can use semaphore as a lock where we initialize the semaphore with X as 1.
+ * sem_init($s, 0, X) where X = 1 which will act as a lock to protect critical section.*/
+#include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+int count = 0; // shared resource
+
+typedef struct __counter {
+  sem_t s;
+} counter_t;
+
+void *counter(void *arg) {
+
+  count++;
+  return NULL;
+}
+
+void spwan_thread(int n) {
+  pthread_t *th = malloc(sizeof(*th) * n);
+  for (int i = 0; i < 5; i++) {
+    pthread_create(&th[i], NULL, counter, NULL);
+  }
+  for (int i = 0; i < 5; i++) {
+    pthread_join(th[i], NULL);
+  }
+}
 
 int main(void) {
   sem_t s;
@@ -10,7 +34,4 @@ int main(void) {
    * Second parameter is 0 indicating that this semaphore will be
    * used within the threads created from the same process.*/
   sem_init(&s, 0, 1);
-  int p = sem_wait(&s);
-  int q = sem_post(&s);
-  printf("p: %d, q: %d\n", p, q);
 }
