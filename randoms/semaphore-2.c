192ca2dad2becf0468a014dbacdd405917130bfb
aatbip <anantabipal2010@gmail.com>
2025-12-12 12:53:55 +0545

    Add basic consumer thread

--- randoms/semaphore-2.c
+++ randoms/semaphore-2.c
@@ -1,6 +1,7 @@
 /* Here we solve the producer/consumer (bounded buffer) problem using semaphore.
  * This program creates a producer/consumer buffer using semaphore.*/
 #include <semaphore.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #define MAX 10
@@ -12,11 +13,16 @@ int cc = 0; // consumer count
 sem_t p, c;
 
 void *producer(void *arg) {
-
   buffer[pc] = rand();
   pc = (pc + 1) % MAX;
   pc++;
+  return NULL;
+}
 
+void *consumer(void *arg) {
+  int tmp = buffer[cc];
+  cc = (cc + 1) % MAX;
+  printf("consumed: %d\n", tmp);
   return NULL;
 }
 
