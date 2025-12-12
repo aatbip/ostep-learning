64b9107b2eee22df7ae3ad53255599c655f8e9a2
aatbip <anantabipal2010@gmail.com>
2025-12-12 13:16:40 +0545

    Add producer and consumer thread and run them

--- randoms/semaphore-2.c
+++ randoms/semaphore-2.c
@@ -1,5 +1,6 @@
 /* Here we solve the producer/consumer (bounded buffer) problem using semaphore.
  * This program creates a producer/consumer buffer using semaphore.*/
+#include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,13 +25,21 @@ int get() {
 }
 
 void *producer(void *arg) {
+  sem_wait(&p);
+  sem_wait(&mutex); // add lock
   put(rand());
+  sem_post(&mutex);
+  sem_post(&c);
   return NULL;
 }
 
 void *consumer(void *arg) {
+  sem_wait(&c);
+  sem_wait(&mutex); // add lock
   int tmp = get();
   printf("consumed: %d\n", tmp);
+  sem_post(&mutex);
+  sem_post(&p);
   return NULL;
 }
 
@@ -38,5 +47,19 @@ int main(void) {
   sem_init(&p, 0, MAX);
   sem_init(&c, 0, 0);
   sem_init(&mutex, 0, 1);
+  pthread_t *th_p = malloc(sizeof(*th_p) * 10);
+  pthread_t *th_c = malloc(sizeof(*th_p) * 10);
+  for (int i = 0; i < 10; i++) {
+    pthread_create(&th_p[i], NULL, producer, NULL);
+  }
+  for (int i = 0; i < 10; i++) {
+    pthread_create(&th_c[i], NULL, consumer, NULL);
+  }
+  for (int i = 0; i < 10; i++) {
+    pthread_join(th_p[i], NULL);
+  }
+  for (int i = 0; i < 10; i++) {
+    pthread_join(th_c[i], NULL);
+  }
   return 0;
 }
