e55229ab5b9dc76cb88391675f5bae870a1ab9e2
aatbip <anantabipal2010@gmail.com>
2025-12-12 12:51:03 +0545

    Add buffer and other variables and begin producer thread

--- randoms/semaphore-2.c
+++ randoms/semaphore-2.c
@@ -1,1 +1,23 @@
-/* Here we solve the producer/consumer (bounded buffer) problem using semaphore. */
+/* Here we solve the producer/consumer (bounded buffer) problem using semaphore.
+ * This program creates a producer/consumer buffer using semaphore.*/
+#include <semaphore.h>
+#include <stdlib.h>
+
+#define MAX 10
+
+int buffer[MAX];
+int pc = 0; // producer count
+int cc = 0; // consumer count
+
+sem_t p, c;
+
+void *producer(void *arg) {
+
+  buffer[pc] = rand();
+  pc = (pc + 1) % MAX;
+  pc++;
+
+  return NULL;
+}
+
+int main(void) { return 0; }
