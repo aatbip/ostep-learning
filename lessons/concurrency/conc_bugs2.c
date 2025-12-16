bd3e75439c526b93386b4c6f4065c3bd537d6c1c
aatbip <anantabipal2010@gmail.com>
2025-12-16 17:12:37 +0545

    Add all Coffman's conditoins for dead lock

--- lessons/concurrency/conc_bugs2.c
+++ lessons/concurrency/conc_bugs2.c
@@ -1,4 +1,13 @@
-/* This code demonstrates deadlock. */
+/* This code demonstrates deadlock.
+ * All 4 of the Coffman's conditions should meet for a deadlock to occur.
+ * 1. Mutual exclusion - We have added mutex_i and mutex_j.
+ * 2. Hold-and-wait - Both t1 and t2 holds the mutex and wait until
+ * they complete incrementing the variables i and j.
+ * 3. No preemption - Mutex are not unlocked from anywhere else. Or there
+ * are no conditions like `trylock` to check if mutex was lock and then recover.
+ * 4. Circular wait - t2 wait for t1 to unlock mutex_i and t1 waits for t2 to unlock
+ * mutex_j. There is a circular wait.*/
+
 #include <pthread.h>
 #include <stdio.h>
 
@@ -11,6 +20,11 @@ pthread_mutex_t mutex_j = PTHREAD_MUTEX_INITIALIZER;
 void *t1(void *arg) {
   pthread_mutex_lock(&mutex_i);
   i += 1;
+  /* We add a long loop so that kernel will context switch at this point.
+   * This context switch will satisfy Coffman's condition of circular wait
+   * for deadlock.*/
+  for (int i = 0; i < 100000; i++)
+    ;
   pthread_mutex_lock(&mutex_j);
   j += 1;
   pthread_mutex_unlock(&mutex_j);
@@ -33,7 +47,7 @@ void *t2(void *arg) {
 int main(void) {
   pthread_t th1, th2;
   pthread_create(&th1, NULL, t1, NULL);
-  pthread_create(&th2, NULL, t1, NULL);
+  pthread_create(&th2, NULL, t2, NULL);
 
   pthread_join(th1, NULL);
   pthread_join(th2, NULL);
