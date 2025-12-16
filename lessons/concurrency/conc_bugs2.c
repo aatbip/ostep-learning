42690aaf2ecbdee8dd53a9868d6214ae1838136e
aatbip <anantabipal2010@gmail.com>
2025-12-16 16:53:11 +0545

    Add threads

---
+++ lessons/concurrency/conc_bugs2.c
@@ -0,0 +1,33 @@
+/* This code demonstrates deadlock. */
+#include <pthread.h>
+#include <stdio.h>
+
+int i = 0;
+int j = 0;
+
+pthread_mutex_t mutex_i = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_j = PTHREAD_MUTEX_INITIALIZER;
+
+void *t1(void *arg) {
+  pthread_mutex_lock(&mutex_i);
+  i += 1;
+  pthread_mutex_lock(&mutex_j);
+  j += 1;
+  pthread_mutex_unlock(&mutex_j);
+  pthread_mutex_unlock(&mutex_i);
+
+  return NULL;
+}
+
+void *t2(void *arg) {
+  pthread_mutex_lock(&mutex_j);
+  j += 1;
+  pthread_mutex_lock(&mutex_i);
+  i += 1;
+  pthread_mutex_unlock(&mutex_i);
+  pthread_mutex_unlock(&mutex_j);
+
+  return NULL;
+}
+
+int main(void) {}
