d78762ccd740ec0977a988fa2badb67822484c77
aatbip <anantabipal2010@gmail.com>
2025-12-16 15:02:42 +0545

    Add routine for t1

--- lessons/concurrency/conc-bugs.c
+++ lessons/concurrency/conc-bugs.c
@@ -1,1 +1,14 @@
 /* This program demonstrates the atomicity violation bug. */
+#include <stdio.h>
+
+typedef struct sys {
+  int node;
+} sys_t;
+
+void *t1(void *arg) {
+  sys_t *sys = (sys_t *)arg;
+  printf("node: %d\n", sys->node);
+  return NULL;
+}
+
+int main(void) { sys_t sys; }
