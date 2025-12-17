7fe778a1a3b1e72727a822cccf2271b0b4476eed
aatbip <anantabipal2010@gmail.com>
2025-12-17 15:37:55 +0545

    singla handler

---
+++ lessons/concurrency/signal.c
@@ -0,0 +1,11 @@
+#include <signal.h>
+#include <stdio.h>
+
+void handle(int arg) { printf("stop wakin’ me up...\n"); }
+
+int main(int argc, char *argv[]) {
+  signal(SIGHUP, handle);
+  while (1)
+    ; // doin’ nothin’ except catchin’ some sigs
+  return 0;
+}
