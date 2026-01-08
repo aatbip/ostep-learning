788bf9575a09a17b05f54b0105eaa611982c6117
aatbip <anantabipal2010@gmail.com>
2026-01-08 13:30:27 +0545

    create links-test directory to put files and their hard links

--- lessons/persistence/fs5.c
+++ lessons/persistence/fs5.c
@@ -1,3 +1,10 @@
 /*This program creates hard links for a file and checks for the number of links before unlinking them. */
+#include <stdio.h>
+#include <sys/stat.h>
 
-int main(void) {}
+int main(void) {
+  int ch = mkdir("links-test", 0777);
+  if (ch != 0) {
+    perror("mkdir");
+  }
+}
