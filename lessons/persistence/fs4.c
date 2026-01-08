049775fce03e15bf8b1dd5436d6ec8f4bd069dd0
aatbip <anantabipal2010@gmail.com>
2026-01-08 11:04:12 +0545

    getting file stat using dirent and stat

--- lessons/persistence/fs4.c
+++ lessons/persistence/fs4.c
@@ -1,12 +1,22 @@
 #include <assert.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 int main(void) {
   DIR *dp = opendir("mydir");
-  assert(dp != NULL);
   struct dirent *d;
-  d = readdir(dp);
-  printf("name: %s\n", d->d_name);
+  while ((d = readdir(dp)) != NULL) {
+    struct stat st;
+    char path[1024];
+    snprintf(path, sizeof(path), "mydir/%s", d->d_name);
+    stat(path, &st);
+    printf("(dirent)name: %s\n", d->d_name);
+    printf("(st)inode: %ld\n", st.st_ino);
+    printf("(dirent)inode: %ld\n", d->d_ino);
+  }
+  closedir(dp);
   return 0;
 }
