4e55493e56a103fa5f2ce2865f86aab200d47cab
aatbip <anantabipal2010@gmail.com>
2026-01-08 13:48:08 +0545

    creates multiple hard links of file0

--- lessons/persistence/fs5.c
+++ lessons/persistence/fs5.c
@@ -1,10 +1,26 @@
 /*This program creates hard links for a file and checks for the number of links before unlinking them. */
+#include <fcntl.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 int main(void) {
   int ch = mkdir("links-test", 0777);
-  if (ch != 0) {
+  if (ch == -1) {
     perror("mkdir");
+  }
+  ch = open("links-test/file0", O_CREAT | O_WRONLY);
+  if (ch == -1) {
+    perror("open");
+  }
+  int i = 1;
+  while (i <= 5) {
+    char n[256];
+    snprintf(n, sizeof(n), "links-test/file%d", i);
+    ch = link("links-test/file0", n);
+    if (ch == -1) {
+      perror("link");
+    }
+    i++;
   }
 }
