c6325c6228f7771302da6f9f05a65fd1cc948b46
aatbip <anantabipal2010@gmail.com>
2026-06-23 19:37:52 +0545

    set id and tag calc

--- lessons/memory-hierarchy/prog1.c
+++ lessons/memory-hierarchy/prog1.c
@@ -5,7 +5,10 @@ int main(void) {
   int s = 3, b = 6;
   uint64_t addr = 0x1A3F5048;
 
-  int offset = addr & ((1 << b) - 1); // low b bits
+  int offset = addr & ((1 << b) - 1);         // low b bits
+  int set_idx = (addr >> b) & ((1 << s) - 1); // next s bits
+  int tag = addr >> (b + s);                  // remaining bits
 
+  printf("offset=%d set=%d tag=%d\n", offset, set_idx, tag);
   return 0;
 }
